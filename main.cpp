44cc517ac3e27cf700c0fb22e2a94eca6f2936ba
zombieTDV <zombietdv2701@gmail.com>
2025-08-28 08:59:17 +0700

    -Xóa các comments không cần thiết trong main.cpp -Comment đoạn code để test câu 10: addEmp(head)

--- main.cpp
+++ main.cpp
@@ -1,19 +1,6 @@
 #include "Linked_list.h"
 
 int main(){
-    // Node* head = new Node{
-    //     {"E001","Alice",{1,1,1990},"alice@mail.com","123 Street","0123456789",20,100.0f}, nullptr
-    // };
-    // head->next = new Node{
-    //     {"E002","Bob",{2,2,1992},"bob@mail.com","456 Avenue","0987654321",18,120.0f}, nullptr
-    // };
-
-    // writeListToFile(head, "employees.txt");
-
-    // Node* listFromFile = readListFromFile("employees.txt");
-
-    // cout << listFromFile->data.employeeID << '\n';
-    // cout << listFromFile->next->data.employeeID << '\n';
     Node* head = NULL;
     string S = "  nguyen  VAN   aN   ";
     standardize(S);
