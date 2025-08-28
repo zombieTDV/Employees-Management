cfaffbba4204b9617c22bfbb69ca5cb90002f26b
zombieTDV <zombietdv2701@gmail.com>
2025-08-28 10:25:51 +0700

    hotfix standardize, bỏ tham chiếu

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main(){
     Node* head = NULL;
     string S = "  nguyen  VAN   aN   ";
-    standardize(S);
+    S = standardize(S);
     cout << S << '\n';
     // addEmp(head);
     return 0;
