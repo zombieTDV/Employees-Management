fec2a6fdeb0d3e398e5e863b07542f17f06d9f79
zombieTDV <zombietdv2701@gmail.com>
2025-08-30 07:22:12 +0700

    -Dọn dẹp lại main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,9 @@
 int main(){
     Node *head = NULL;
     menu(head);
+
+
     head = new Node{{"E001","Alice",{1,1,1990},"alice@mail.com","123 Street","0123456789",20,100.0f}, nullptr};
-    string S = "  nguyen  VAN   aN   ";
-    S = standardize(S);
-    cout << S << '\n';
     cout << "   THEM MOI NV" << endl;
     addEmp(head);
 
